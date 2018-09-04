8394fd52949e562078eac1d7eb977d4aee4a6152
jasonduquain <jduquain@gmail.com>
2018-09-04 10:00:16 -0400

    bloc-chat-create-chat-rooms_checkpoint: complete

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,6 +4,8 @@ class RoomList extends Component {
     constructor(props) {
         super(props);
         this.roomsRef = this.props.firebase.database().ref('rooms');
+        this.showModal = this.showModal.bind(this);
+        this.createRoom = this.createRoom.bind(this);
         this.state = {
             rooms: []
         };
@@ -18,18 +20,47 @@ class RoomList extends Component {
        });
    }
     
+    showModal() {
+        document.querySelector('.roomlist__modal').classList.add('show');
+    }
+    
+    // 20180904 - enter more validation in future and also validate new room doesnt already exist - this might be part of a future checkpoint so not yet
+    createRoom(e) {
+        e.preventDefault();
+        const roomName = e.target.elements.roomlist_input.value
+        if (roomName) {
+              this.roomsRef.push({
+              name: roomName
+            });
+            e.target.elements.roomlist_input.value = '';
+        } else {
+            alert('please enter a room name');
+        }
+    }
+    
   render() {
     return (
         <div className="roomlist">
             <div className="roomlist__sidebar">
                 <h2 className="roomlist__sidebar-heading">Bloc Chat</h2>
+                <button className="roomlist__sidebar-btn" onClick={this.showModal}>New room</button>
                <ul className="roomlist__list">
                   {
                        this.state.rooms.map((el, idx) => <li key={el.key} className="roomlist__item">{el.name}</li>)
                   }
                </ul>
             </div>
-            <div className="roomlist__main"></div>
+            <div className="roomlist__main">
+                 <div className="roomlist__modal">
+                     <h3>Create new room</h3>
+                    <form className="roomlist__form" onSubmit={this.createRoom}>
+                        <label htmlFor="roomlist_input">Enter a room Name</label>
+                        <input type="text" id="roomlist_input" name="roomlist_input"/>
+                        <button>Cancel</button>
+                        <button>Create Room</button>
+                    </form>
+                </div>   
+            </div>
         </div>
     );
   }
